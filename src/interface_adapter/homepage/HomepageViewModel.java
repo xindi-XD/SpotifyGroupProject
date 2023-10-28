ede88a2f294230b2f57d64562d0319b67f0292a9
xindi-XD <taoxd424527@163.com>
2023-10-28 00:26:42 -0400

    Wrote basic structure for HomepageView (errors mean unimplemented). Added additional abstract class ViewModel, PlaylistCreationFailed, and ViewManagerModel under the interface_adapter folder. Wrote parts of HomepageViewModel and CreatePlaylistViewModel.

--- src/interface_adapter/homepage/HomepageViewModel.java
+++ src/interface_adapter/homepage/HomepageViewModel.java
@@ -1,4 +1,50 @@
 package interface_adapter.homepage;
 
-public class HomepageViewModel {
+import interface_adapter.ViewModel;
+import interface_adapter.create_playlist.CreatePlaylistState;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+public class HomepageViewModel extends ViewModel {
+    public static final String TITLE_LABEL = "Homepage View";
+    public static final String CREATEPLAYLIST_BUTTON_LABEL = "Create Playlist";
+    // TODO: show, delete and search to be implemented.
+    public static final String SHOWPLAYLISTS_BUTTON_LABEL = "Show Playlists";
+    public static final String DELETEPLAYLIST_BUTTON_LABEL = "Delete Playlist";
+    public static final String SEARCH_LABEL = "Search for song/artist";
+
+    private HomepageState state = new HomepageState();
+
+    /**
+     * I'm not sure what super means. Is this the head view model for the view? */
+    public HomepageViewModel(){
+        super("homepage");
+    }
+
+
+    @Override
+    public void firePropertyChanged() {
+        support.firePropertyChange("state", null, this.state);
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        support.addPropertyChangeListener(listener);
+    }
+    /**
+     * Methods below aren't inherited from ViewModel. Might delete. */
+    public void setState(HomepageState state) {
+        this.state = state;
+    }
+
+    private final PropertyChangeSupport support = new PropertyChangeSupport(this);
+
+    public HomepageState getHomepageState() {
+        return state;
+    }
+
+    public CreatePlaylistState getCreatePlaylistState() {
+        return null;
+    }
 }
