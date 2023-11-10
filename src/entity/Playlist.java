f79c67cf647975ef08a2d4c030f6c19119d81f38
DavidChu <xiaoshuai.chu@mail.utoronto.ca>
2023-11-10 15:57:10 -0500

    Added attributes to Playlist file.

--- src/entity/Playlist.java
+++ src/entity/Playlist.java
@@ -1,4 +1,13 @@
 package entity;
 
+import java.util.Map;
+
 public class Playlist {
+    private String name;
+    private Map<String, Song> songs;
+    private int length;
+    private boolean privacy;
+    private String genres;
+    private String artists;
+    private boolean saved;
 }
