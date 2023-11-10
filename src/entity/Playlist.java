5c0148e4c55571cef9273510607f0fbd6b1ef7bc
DavidChu <xiaoshuai.chu@mail.utoronto.ca>
2023-11-10 16:45:41 -0500

    Adjust some attributes on Playlist file and create setters and getters.

--- src/entity/Playlist.java
+++ src/entity/Playlist.java
@@ -1,13 +1,38 @@
 package entity;
 
-import java.util.Map;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
 
 public class Playlist {
     private String name;
-    private Map<String, Song> songs;
-    private int length;
-    private boolean privacy;
-    private String genres;
-    private String artists;
-    private boolean saved;
+    private HashMap<String, Song> songs;
+    private boolean privacy; // true when is private, default false.
+
+    public Playlist(String name) {
+        this.name = name;
+        this.songs = new HashMap<>();
+        this.privacy = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLength() {
+        return songs.size();
+    }
+
+    public boolean getPrivacy() {
+        return privacy;
+    }
+
+    public void setPrivacy() {
+        this.privacy = !privacy;
+    }
 }
