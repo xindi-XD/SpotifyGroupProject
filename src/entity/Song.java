dd1f4098fb58a8deec69d54a27ecfea50e22fc2b
DavidChu <xiaoshuai.chu@mail.utoronto.ca>
2023-11-10 16:21:40 -0500

    Added attributes, constructor, and getters for Song.

--- src/entity/Song.java
+++ src/entity/Song.java
@@ -1,4 +1,25 @@
 package entity;
 
 public class Song {
+    private String name;
+    private String artist;
+    private int id;
+
+    public Song(String name, String artist, int id) {
+        this.name = name;
+        this.artist = artist;
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getArtist() {
+        return this.artist;
+    }
+
+    public int getId() {
+        return this.id;
+    }
 }
