e21be21a9e85f4669d8f2f55a2ac58c164c9e20c
DavidChu <xiaoshuai.chu@mail.utoronto.ca>
2023-11-10 16:39:40 -0500

    Adjust some attributes on Song file.

--- src/entity/Song.java
+++ src/entity/Song.java
@@ -4,22 +4,28 @@ public class Song {
     private String name;
     private String artist;
     private int id;
+    private String genre;
 
-    public Song(String name, String artist, int id) {
+    public Song(String name, String artist, int id, String genre) {
         this.name = name;
         this.artist = artist;
         this.id = id;
+        this.genre = genre;
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
 
     public String getArtist() {
-        return this.artist;
+        return artist;
     }
 
     public int getId() {
-        return this.id;
+        return id;
+    }
+
+    public String getGenre() {
+        return genre;
     }
 }
