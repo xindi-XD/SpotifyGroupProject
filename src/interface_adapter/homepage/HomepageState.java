fc51c1b2d69f818c246ae5690bf27f0523eb492b
jayuj <jasminewei082@gmail.com>
2023-11-13 19:42:19 -0500

    Implemented homepagestate

--- src/interface_adapter/homepage/HomepageState.java
+++ src/interface_adapter/homepage/HomepageState.java
@@ -1,4 +1,19 @@
 package interface_adapter.homepage;
 
+import entity.Playlist;
+
+import java.util.ArrayList;
+
 public class HomepageState {
+    private String homepageError = null;
+
+    public HomepageState(HomepageState copy) {
+        this.homepageError = copy.homepageError;
+    }
+
+    public HomepageState() {}
+
+    public String getHomepageError() {return this.homepageError;}
+
+    public void setHomepageError(String error) {this.homepageError = error;}
 }
