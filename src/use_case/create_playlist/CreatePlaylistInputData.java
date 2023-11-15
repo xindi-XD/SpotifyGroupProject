794266c23452380114890a481f92af17a19c51c6
DavidChu <xiaoshuai.chu@mail.utoronto.ca>
2023-11-14 20:02:17 -0500

    Made some minor changes to some files.

--- src/use_case/create_playlist/CreatePlaylistInputData.java
+++ src/use_case/create_playlist/CreatePlaylistInputData.java
@@ -1,4 +1,10 @@
 package use_case.create_playlist;
 
 public class CreatePlaylistInputData {
+    final private String name;
+
+
+    public CreatePlaylistInputData(String name) {
+        this.name = name;
+    }
 }
