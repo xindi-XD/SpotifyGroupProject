0d5cd98c0a49386fbb6d95d1a74df8a0ae2ad01c
DavidChu <xiaoshuai.chu@mail.utoronto.ca>
2023-11-24 17:45:16 -0500

    To be able to create a json file.

--- src/data_access/FilePlaylistDataAccessObject.java
+++ src/data_access/FilePlaylistDataAccessObject.java
@@ -1,10 +1,13 @@
 package data_access;
 
 import entity.CommonPlaylist;
+import entity.CommonPlaylistFactory;
 import entity.PlaylistFactory;
 import org.json.JSONObject;
 import use_case.create_playlist.CreatePlaylistDataAccessInterface;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -15,14 +18,14 @@ public class FilePlaylistDataAccessObject implements CreatePlaylistDataAccessInt
     private final Map<String, CommonPlaylist> playlists = new HashMap<>();
     private PlaylistFactory playlistFactory;
 
-    public FilePlaylistDataAccessObject(String jsonPath, PlaylistFactory playlistFactory) {
+    public FilePlaylistDataAccessObject(String jsonPath, PlaylistFactory playlistFactory) throws IOException {
         this.playlistFactory = playlistFactory;
 
-        jsonFile = new JSONObject(jsonPath);
-
-        if (jsonFile.length() == 0) {
-            save();
-        }
+        jsonFile = new JSONObject();
+        jsonFile.put("LadyGaGa", new CommonPlaylistFactory());
+        FileWriter file = new FileWriter(jsonPath);
+        file.write(jsonFile.toString());
+        file.close();
     }
 
     @Override
