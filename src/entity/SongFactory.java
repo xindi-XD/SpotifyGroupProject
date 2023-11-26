2d42a50a6044333038784ecd318394b06addd55e
jayuj <jasminewei082@gmail.com>
2023-11-26 05:30:25 -0500

    Working on GetTrack

--- src/entity/SongFactory.java
+++ src/entity/SongFactory.java
@@ -13,18 +13,29 @@ public class SongFactory {
         return new Song(name, artist, id);
     }
 
+    public static Song create(JSONObject object) {
+        return new Song(object.getString("name"),
+                parseArtists(object.getJSONArray("artists")).toArray(new String[0]),
+                object.getString("id"));
+    }
+
     public static ArrayList<Song> createSongList(JSONArray results) {
         ArrayList<Song> songs = new ArrayList<>();
         for (int i = 0; i < results.length(); i++) {
             JSONObject track = results.getJSONObject(i);
-            ArrayList<String> artists = new ArrayList<>();
             JSONArray artistObjects = track.getJSONArray("artists");
-            for (int j = 0; j < artistObjects.length(); j++) {
-                artists.add(artistObjects.getJSONObject(j).getString("name"));
-            }
+            ArrayList<String> artists = parseArtists(artistObjects);
             Song song = create(track.getString("name"), artists.toArray(new String[0]), track.getString("id"));
             songs.add(song);
         }
         return songs;
+    }
+
+    public static ArrayList<String> parseArtists(JSONArray artistJSON) {
+        ArrayList<String> artists = new ArrayList<>();
+        for (int j = 0; j < artistJSON.length(); j++) {
+            artists.add(artistJSON.getJSONObject(j).getString("name"));
+        }
+        return artists;
     }
 }
