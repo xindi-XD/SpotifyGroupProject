0088fb626450f37c9cc979861852195efb07291c
jayuj <jasminewei082@gmail.com>
2023-12-02 22:37:04 -0500

    JSONObject error fix

--- test/TestAPI.java
+++ test/TestAPI.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import data_access.APIDataAccessObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class TestAPI {
     @Test
@@ -16,11 +17,9 @@ public class TestAPI {
         assert searchTrackResults.length() == 20;
         searchTrackResults = apiDataAccessObject.search("abc", "track", 10);
         assert searchTrackResults.length() == 10;
-
-        CommonSongFactory songFactory = new CommonSongFactory();
         ArrayList<CommonSong> returnedSongs = new ArrayList<>();
         for (int i = 0; i < searchTrackResults.length(); i++) {
-            returnedSongs.add(songFactory.create(searchTrackResults.getJSONObject(i)));
+            returnedSongs.add(CommonSongFactory.create(searchTrackResults.getJSONObject(i)));
             assert !returnedSongs.get(i).getID().isEmpty();
             System.out.println(returnedSongs.get(i).getName());
         }
@@ -30,12 +29,23 @@ public class TestAPI {
     public void testGetTrack() {
         APIDataAccessObject apiDataAccessObject = new APIDataAccessObject();
         JSONObject songResult = apiDataAccessObject.getTrack("6kyHcHEBPtwjEbUWrNuLlv");
-        CommonSongFactory songFactory = new CommonSongFactory();
-        CommonSong song = songFactory.create(songResult);
+        CommonSong song = CommonSongFactory.create(songResult);
         assert song.getName().equals("Among Us (Trap Remix)");
         assert song.getID().equals("6kyHcHEBPtwjEbUWrNuLlv");
         assert song.getArtist().length == 1;
         assert song.getArtist()[0].equals("Leonz");
+    }
+
+    @Test
+    public void testGetFeatures() {
+        APIDataAccessObject apiDataAccessObject = new APIDataAccessObject();
+        HashMap<String, Float> result = apiDataAccessObject.getTrackFeatures("6kyHcHEBPtwjEbUWrNuLlv");
+        String[] featureNames = {"acousticness", "danceability", "energy", "instrumentalness", "liveness",
+                "loudness", "speechiness", "valence"};
+        for (String feature : featureNames) {
+            assert result.get(feature) != null;
+            System.out.println(result.get(feature));
+        }
     }
 
 }
